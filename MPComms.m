25a921d72a94930e66a7481a19654b2431a07a16
Jay Baker <logikal@gmail.com>
2015-07-06 11:38:08 +1200

    adding dictionary of callbacks

--- MPComms.m
+++ MPComms.m
@@ -1,13 +1,21 @@
 #import "MPComms.h"
 
 @implementation MPComms
-void (^callback)(NSNumber *, NSData*);
+NSMutableDictionary *commands;
+
+-(id)init
+{
+    if (self = [super init]) {
+        commands = [[NSMutableDictionary alloc] init];
+    }
+    
+    return self;
+}
 
 -(void)sendMessage:(NSNumber *)messageId toPort:(NSString *)port withData:(NSData *)data
 {
     CFStringRef port_name = (__bridge CFStringRef)port;
     CFMessagePortRef mPort = CFMessagePortCreateRemote(kCFAllocatorDefault, port_name);
-    
     if (mPort != nil) {
         CFDataRef response = NULL;
         CFMessagePortSendRequest(mPort, (SInt32)[messageId intValue], (__bridge CFDataRef)data, 1000, 1000, kCFRunLoopDefaultMode, &response);
@@ -22,16 +30,21 @@ void (^callback)(NSNumber *, NSData*);
 
 CFDataRef message_callback(CFMessagePortRef local, SInt32 msgid, CFDataRef data, void *info)
 {
-    callback([NSNumber numberWithInt:msgid], (__bridge NSData*)data);
+    void (^cb)(NSNumber *, NSData*) = [commands objectForKey:[NSNumber numberWithInt:msgid]];
+    if (cb != nil) {
+        cb([NSNumber numberWithInt:msgid], (__bridge NSData*)data);
+    }
+    
     return NULL;
 }
 
 -(void)recvMessage:(NSNumber *)messageId fromPort:(NSString *)port withCallback:(void(^)(NSNumber *, NSData *))cb
 {
-    callback = cb;
     CFStringRef port_name = (__bridge CFStringRef)port;
     CFMessagePortRef mPort = CFMessagePortCreateLocal(kCFAllocatorDefault, port_name, &message_callback, NULL, NULL);
     CFMessagePortSetDispatchQueue(mPort, dispatch_get_main_queue());
+    
+    [commands setObject:cb forKey:messageId];
 }
 
 @end
